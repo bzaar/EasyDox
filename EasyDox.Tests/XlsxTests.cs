836f1acbbf0d9a748af8b70ede7362577eed0906
bzaar <slepov@gmail.com>
2017-03-12 12:01:55 +0000

    Remove unused usings

--- EasyDox.Tests/XlsxTests.cs
+++ EasyDox.Tests/XlsxTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Text.RegularExpressions;
 using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
